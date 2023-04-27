a3367adf6a6d3b4b9ef29e63c3556e83ac39d17c
Carlos Daniel Vargas Varela <carlos.vargasvarela@ucr.ac.cr>
2023-04-27 11:45:38 -0600

    Se finaliza la GUI del insert de actas hasta los trámites

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -1,26 +1,20 @@
-import {
-    add,
-    Controller
-} from "@hotwired/stimulus"
+import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = ["add_item"];
-
-    items_added = 0;
 
     add_association(event) {
-        this.items_added += 1;
+        event.preventDefault();
         const level = event.target.dataset.level;
         const template = document.getElementById(`template${level}`);
-        const add_item = document.getElementById(`addItem${level}`);
-        console.log(template);
-        console.log(add_item);
-        console.log(this.items_added)
-        event.preventDefault();
-        let content = '<div>';
-        content += template.innerHTML.replace(/TEMPLATE_RECORD/g, this.items_added.toString());
-        content += '</div>';
-        add_item.insertAdjacentHTML('afterend', content);
+        let container;
+        if (level === "1") {
+            container = event.target.parentElement;
+        } else {
+            container = event.target.closest(".nested-fields").parentElement;
+        }
+        let childrenDiv = container.querySelector(".childrenContainer");
+        const totalChildren = childrenDiv.childElementCount + 1;
+        this.createChildren(childrenDiv, level, totalChildren, template);
     }
 
     remove_association(event) {
@@ -28,6 +22,52 @@ export default class extends Controller {
         let item = event.target.closest(".nested-fields");
         item.querySelector("input[name*='_destroy']").value = 1;
         item.style.display = 'none';
+        let childrenDiv = item.parentElement.parentElement.querySelector(".childrenContainer");
+        if (childrenDiv) {
+            let children = childrenDiv.children;
+            for (let i = 0; i < children.length; i++) {
+                children[i].style.display = 'none';
+                children[i].querySelector("input[name*='_destroy']").value = 1;
+            }
+        }
     }
 
+    create_id(childrenDiv, level, totalChildren) {
+        if (level > 1) {
+            let parentDiv = childrenDiv.parentElement;
+            let parentId = parentDiv.id;
+            let children = parentDiv.childElementCount + 1;
+            return parentId + "-" + children;
+        } else {
+            return level + "-" + totalChildren;
+        }
+    }
+
+    addChildrenDiv(children, level, totalChildren) {
+        let parentDiv = children.parentElement.parentElement;
+        console.log(parentDiv);
+        let parentId = parentDiv.id;
+        if (level === "1") {
+            parentId = 1;
+        }
+        let newChild = document.createElement('section');
+        newChild.id = `cl-${parentId}-${totalChildren}`;
+        newChild.classList.add('childrenContainer');
+        newChild.classList.add('row');
+        children.appendChild(newChild);
+        console.log(children);
+    }
+
+    createChildren(childrenDiv, level, totalChildren, template) {
+        let newChild = document.createElement('div');
+        newChild.classList.add('row');
+        newChild.classList.add(`ms-${level}`);
+        newChild.classList.add('mt-1');
+        newChild.classList.add('mb-1');
+        newChild.id = this.create_id(childrenDiv, level, totalChildren);
+        newChild.innerHTML = template.innerHTML.replace(/TEMPLATE_RECORD/g, newChild.id);
+        childrenDiv.appendChild(newChild);
+        this.addChildrenDiv(newChild, level, totalChildren);
+        return newChild;
+    }
 }
