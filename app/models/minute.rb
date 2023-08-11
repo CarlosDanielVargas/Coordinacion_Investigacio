569caecd94d44432e88271b503e26750e8fdfaa0
Carlos Daniel Vargas Varela <carlos.vargasvarela@ucr.ac.cr>
2023-08-11 11:39:57 -0600

    Se agrega listado de actas

--- app/models/minute.rb
+++ app/models/minute.rb
@@ -1,6 +1,6 @@
 class Minute < ApplicationRecord
     has_one_attached :file
-    has_many :projects, through: :articles
     has_many :articles, dependent: :destroy
+    has_many :projects, through: :articles
     accepts_nested_attributes_for :articles, allow_destroy: true, reject_if: proc { |attr| attr["code"].blank? }
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -3,8 +3,8 @@ class Project < ApplicationRecord
   
   has_many :project_investigators
   has_many :investigators, through: :project_investigators
-  has_many :minutes, through: :articles
   has_many :articles, dependent: :destroy
+  has_many :minutes, through: :articles
 
   accepts_nested_attributes_for :project_investigators, reject_if: ->(attributes) { attributes["name"].blank? }, allow_destroy: true
 
