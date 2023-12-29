a56aae42f7d83a251f4ef28e575f2797a6cd3929
Carlos Daniel Vargas Varela <carlos.vargasvarela@ucr.ac.cr>
2023-12-29 10:43:37 -0600

    Se hacen correcciones en las migraciones

--- db/migrate/20230904231850_change_nulleable_to_not_nulleable_collumns.rb
+++ db/migrate/20230904231850_change_nulleable_to_not_nulleable_collumns.rb
@@ -1,7 +1,7 @@
 class ChangeNulleableToNotNulleableCollumns < ActiveRecord::Migration[7.0]
   def change
     change_column_null :project_investigators, :project_id, false
-    change_column_null :project_investigators, :investigator_id, false
+    #change_column_null :project_investigators, :investigator_id, false
     change_column_null :minutes, :creation_date, false
     change_column_null :notices, :code, false
     change_column_null :articles, :minute_id, false
--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_08_205915) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_08_205915) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -89,8 +89,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_08_205915) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "project_id", null: false
-    t.integer "investigator_id", null: false
-    t.index ["investigator_id"], name: "index_project_investigators_on_investigator_id"
+    t.integer "individual_id"
+    t.index ["individual_id"], name: "index_project_investigators_on_individual_id"
     t.index ["project_id"], name: "index_project_investigators_on_project_id"
   end
 
@@ -148,7 +148,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_08_205915) do
   add_foreign_key "articles", "minutes"
   add_foreign_key "articles", "projects"
   add_foreign_key "notices", "transaction_records"
-  add_foreign_key "project_investigators", "individuals", column: "investigator_id"
+  add_foreign_key "project_investigators", "individuals"
   add_foreign_key "project_investigators", "projects"
   add_foreign_key "requests", "individuals"
   add_foreign_key "transaction_records", "agreements"
