83cda50babc41228a6e4553b1f092231152e6b05
KevinJSS <kevin.alvarado@ucrso.info>
2024-01-07 19:02:15 -0600

    Added the main scss file to group the general styles of every view. And also added custom styles to the will_paginate links.

---
+++ config/initializers/will_paginate.rb
@@ -0,0 +1,2 @@
+WillPaginate::ViewHelpers.pagination_options[:previous_label] = '<i class="bi bi-chevron-left"></i>'
+WillPaginate::ViewHelpers.pagination_options[:next_label] = '<i class="bi bi-chevron-right"></i>'
